3bbba7a38d4a91de68f3b369617a1f2319e57316
AlbertoGA2005 <149528964+AlbertoGA2005@users.noreply.github.com>
2023-11-05 02:16:15 -0600

    Add files via upload

---
+++ Práctica1.java
@@ -0,0 +1,14 @@
+package práctica.pkg1;
+/**
+ *
+ * @author Anahí
+ * Objetivo: Mostrar un mensaje en la pantalla
+ * 28/10/2023
+ */
+public class Práctica1 {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola mi nombre es Dulce Anahí y mis apelidos son Rosado García");
+        System.out.println("Mi nombre junto es Dulce Anahí Rosado García");
+    }
+}
